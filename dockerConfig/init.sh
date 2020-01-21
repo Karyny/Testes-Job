561c74bac489ce6852bad536d38e784049382a5d
Karyny <karyny.rodio@unoesc.edu.br>
2020-01-21 16:43:40 -0200

    first commit

---
+++ dockerConfig/init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "127.0.0.1 fotogea.photobookfinal.com" >> /etc/hosts
+echo "127.0.0.1 modelo.photobookfinal.com" >> /etc/hosts
+echo "127.0.0.1 webphoto.photobookfinal.com" >> /etc/hosts
